7adf511124bc807b5b11d8d7dfddda59c44f6e17
unknown <chinhnguyen.210399@gmail.com>
2020-12-17 16:34:45 +0700

    Edit config file

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php 
-DEFINE('INDEX_URL','http://localhost:1234/');
-DEFINE('APP_URL','http://localhost:1234/WBH0/');
+DEFINE('INDEX_URL','http://localhost/');
+DEFINE('APP_URL','http://localhost/WBH0/');
 DEFINE('BASE_HREF','/WBH0/');
 DEFINE('DB_HOST','localhost');
 DEFINE('DB_USERNAME','root');

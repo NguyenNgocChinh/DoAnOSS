02dce24b30e9a90f142a841c2ef51580e415331d
DangNhuVu <gem041611@gmail.com>
2020-12-23 22:38:59 +0700

    Fix_Admin

--- models/admin/productModel.php
+++ models/admin/productModel.php
@@ -2,22 +2,12 @@
 
 class productModel extends Model
 {
-	/*private $masp, $tensp, $gia;*/
 	function __construct()
 	{
 		parent::__construct();
 	}
-	function getPrds($orderBy, $start, $last, $where = null){
-		if($where === null){
-			$sql = "SELECT * FROM sanpham ORDER BY ".$orderBy." desc LIMIT ".$start.",".$last;
-		} else {
-			$sql = "SELECT * FROM sanpham WHERE ".$where." ORDER BY ".$orderBy." desc LIMIT ".$start.",".$last;
-		}
-		
-		$prd = array();
-		foreach($this->conn->query($sql) as $row){
-			$prd[] = $row;
-		}
+	function getAllPrds(){
+		$prd = $this->select('*', 'sanpham sp, danhmucsp dm','sp.madm = dm.madm', 'ORDER BY sp.ngay_nhap DESC');
 		return $prd;
 	}
 }

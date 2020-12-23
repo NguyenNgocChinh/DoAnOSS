8a07fb3829b4cdf9e676c7c9638fa464cba95025
Đặng Ngọc An <72913911+ngocanstu@users.noreply.github.com>
2020-12-23 22:14:34 +0700

    FIX_BUG_DangKy
    Chỗ điền pass bị thay đổi biến

--- controllers/users/UserController.php
+++ controllers/users/UserController.php
@@ -120,7 +120,7 @@ require_once 'vendor/Model.php';
 			return 0;
 		}
 		if(isset($_POST['password'])){
-			$password = $_POST['passwor'];
+			$password = $_POST['password'];
 		}
 		if(isset($_POST['cpassword'])){
 			$cpassword = $_POST['cpassword'];
